6f96c8eeba785c112daae118e9e7446d96d53b23
anatoliy <omelchenko20@gmail.com>
2022-09-04 18:03:03 +0300

    Changed fixer URL(2)

--- src/main/java/org/example/mespringgeo/retrievers/RateRetriever.java
+++ src/main/java/org/example/mespringgeo/retrievers/RateRetriever.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class RateRetriever {
 
-    private static final String URL = "http://data.fixer.io/api/latest?access_key=K9kolJmhXZdHcc11aZYfW9o50e8QckSo";
+    private static final String URL = "http://data.fixer.io/api/latest?access_key=30e7ff0de73cf99ac8842101abce80ca";
 
     @Cacheable("rates")
     public Rate getRate() {
